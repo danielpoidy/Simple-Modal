c82648bfd07b34378aa89a3512516261afa74100
danielpoidy <poidydaniel@gmail.com>
2020-08-11 15:32:14 +0000

    adding modal functionnality with javascript and little styling enhancing

---
+++ index.js
@@ -0,0 +1,8 @@
+
+document.querySelector('.show').addEventListener('click', function () {
+  document.querySelector('.overlay').style.display = 'block'
+})
+
+document.querySelector('.close').addEventListener('click', function () {
+  document.querySelector('.overlay').style.display = 'none'
+})
